87e1854c55057a65422d8eb846af1029c3e8c8c0
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2023-05-23 16:03:11 +0100

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/Profile-card-component/',
-  base: '/',
+  base: '/Profile-card-component/',
 })
